f437156bc53c0a3c3e8869e656b3380207c90cdd
etff <wish2rich@gmail.com>
2018-11-11 10:38:49 +0900

    tab edit

--- main.js
+++ main.js
@@ -5,23 +5,34 @@
 // 내용 0,1,2에 있던 show-tab라는 클래스가 사라지고 
 // 내용 0에 show-tab 클래스가 붙음.
 
-
-for (let i = 0; i < 3; i++){
-	document.querySelectorAll('.tab-button')[i].addEventListener('click',function(){
-	
+function showButtons(i) {
 	let button = document.querySelectorAll('.tab-button');
-	let content = document.querySelectorAll('.tab-content');
-	
+
 	button[0].classList.remove('active');
 	button[1].classList.remove('active');
 	button[2].classList.remove('active');
 	
 	button[i].classList.add('active');
-	
+}
+
+function showTabs(i) {
+	let content = document.querySelectorAll('.tab-content');
+
 	content[0].classList.remove('show-tab');
 	content[1].classList.remove('show-tab');
 	content[2].classList.remove('show-tab');
 	
 	content[i].classList.add('show-tab');
-});
 }
+
+document.querySelector('.list').addEventListener('click', function(e) {
+	
+	/*
+	*	버튼 0을 눌렀을 경우, 버튼 보이기(0), 탭보이기(0)을 실행
+		버튼 0을 눌렀을 경우, 버튼 보이기(1), 탭보이기(1)을 실행
+		버튼 0을 눌렀을 경우, 버튼 보이기(2), 탭보디기(2)을 실행
+	*/
+	
+	showButtons(e.target.dataset.id);
+	showTabs(e.target.dataset.id);
+});
